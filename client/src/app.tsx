502356e85c067fd2f5701e192f0b9544e7e2c071
Mohammad Helaly <mohammad.helaly@outlook.com>
2026-05-07 01:26:13 +0300

    Remove development notification

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import NavBar from "@/components/nav-bar";
 import Background from "@/components/background";
 import HomeSection from "@/components/home-section";
@@ -6,26 +5,8 @@ import VQnASection from "@/components/vqna-section";
 import AboutSection from "@/components/about-section";
 import ContactSection from "@/components/contact-section";
 import Footer from "@/components/footer";
-import Notification from "@/components/notification";
 
 const App = () => {
-  const [notificationOpen, setNotificationOpen] = useState(false);
-
-  useEffect(() => {
-    const showNotificationTimeout = setTimeout(() => {
-      setNotificationOpen(true);
-    }, 2000);
-
-    const hideNotificationTimeout = setTimeout(() => {
-      setNotificationOpen(false);
-    }, 7000); // 2 seconds delay + 5 seconds display time
-
-    return () => {
-      clearTimeout(showNotificationTimeout);
-      clearTimeout(hideNotificationTimeout);
-    };
-  }, []);
-
   return (
     <>
       <NavBar />
@@ -35,12 +16,6 @@ const App = () => {
         <VQnASection />
         <AboutSection />
         <ContactSection />
-        <Notification
-          text="This web app is still under development. Some features may not be available yet."
-          variant="info"
-          open={notificationOpen}
-          onOpenChange={setNotificationOpen}
-        />
       </main>
       <Footer />
     </>
